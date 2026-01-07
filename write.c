69abe24e610abcd6fa329eb59f1935c7ca69d11d
Moontaha Ariba <aribamoontaha@gmail.com>
2026-01-07 15:32:34 -0500

    began writing a file, but changed plans

---
+++ write.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+// the purpose of this function is to write all the words to words.txt
+// we're going to find the words from a list online
+void writeToFile(char * string) {
+
+}
