5d03b51ff5cca77b1b3d9399f36b0b8f0a43a364
ginvvan <ginwan3lfaki@gmail.com>
2026-01-08 15:08:30 -0500

    moved files

--- makefile
+++
@@ -1,8 +0,0 @@
-compile: main.o write.h
-	gcc main.o -o prog
-main.o: main.c
-	gcc -c main.c
-run: prog
-	./prog
-clean:
-	rm *.o prog
---
+++ wordle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include "write.h"
+
+
+int main(int argc, int *argv[]){
+  char allwords[64] = "berry"
+  char words[64];
+
+  return 0;
+}
