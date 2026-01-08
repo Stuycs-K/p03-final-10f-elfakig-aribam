b4e248002acc795ab17d7653af062fea2b2ffc88
Moontaha Ariba <aribamoontaha@gmail.com>
2026-01-08 15:32:34 -0500

    functions to calculate fileSize and choose a word (incomplete)

--- wordle.c
+++ wordle.c
@@ -1,13 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include "write.h"
+#include "networking.h"
 
+int fileSize() {
+    FILE * fp = fopen("words.csv", "r");
+    if (fp == NULL) {
+        printf("file not found\n");
+        return -1;
+    }
+    fseek(fp, 0, SEEK_END);
+    int s = ftell(fp);
+    fclose(fp);
+    return s;
+}
+
+void chooseWord(char * w) {
+    // compute the number of lines in the file
+    int numWords = fileSize() / 6;
+
+    // choose a random line
+    FILE * f = fopen("words.csv", "r");
+    fgets(w, 5, f);
+    char * line = w;
+    w = "hi";
+}
 
 int main(int argc, int *argv[]){
-  char allwords[64] = "berry"
+  char allwords[64] = "berry";
   char words[64];
+  char w[5];
+
+  printf("%d\n", fileSize());
 
   return 0;
 }
